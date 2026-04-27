11770bed01a25599f8eeeffcf17d3f3c8f23bf78
vdiamar2309 <vdiamar2309>
2026-04-27 13:42:13 +0200

    Mensaje

--- Ejercicio5/src/main/java/main.java
+++ Ejercicio5/src/main/java/main.java
@@ -1,36 +1,53 @@
 import util.MiEntradaSalida;
-
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 
 public class main {
+    public static String acciones = "mkdir -> Crear Directorio\n" +
+            "touch -> Crear Fichero con texto\n" +
+            "ls -> Listar ficheros Carpetas\n" +
+            "Default -> acciones\n" +
+            "acciones -> acciones\n" +
+            "del -> borrar fichero o carpeta";
+
     static void main(String[] args) {
-        boolean run=true;
-        while (run){
+        boolean run = true;
+        while (run) {
             String mensaje = (String) MiEntradaSalida.solicitarCadena("Que acción quiere hacer");
-            switch (mensaje){
+            switch (mensaje) {
                 case "lista" -> {
                     System.out.println(acciones);
                     break;
                 }
                 case "mkdir" -> {
-                    String nombre = (String) MiEntradaSalida.solicitarCadena("¿Que nombre le quiere poner al directorio?");
+                    String nombre = MiEntradaSalida.solicitarCadena("¿Que nombre le quiere poner al directorio?");
                     crearDirectorio(nombre);
                 }
-                case "touch"->{
-
+                case "touch" -> {
+                    String nombre =  MiEntradaSalida.solicitarCadena("¿Que nombre le quiere poner al fichero?");
+                    crearFichero(nombre);
+                }
+                case "del" -> {
+                    String nombre = MiEntradaSalida.solicitarCadena("¿Que fichero o directorio quiere borrar?");
+                    borrarFicheroODirectorio(nombre);
+                }
+                case "ls"->{
+                    String nombre = MiEntradaSalida.solicitarCadena("¿Que fichero o directorio quiere borrar?");
+                    listarFicherosCarpeta(nombre);
                 }
                 default -> System.out.println(acciones);
             }
         }
     }
 
-    public static String acciones = "Bla bla bla aqui van las acciones";
-    public static void crearDirectorio(String directorio){
-        Path p = Paths.get("C:\\Users\\alumno\\IdeaProjects\\ficheros\\Ejercicio5",directorio);
+    public static void crearDirectorio(String directorio) {
+        Path p = Paths.get("C:\\Users\\alumno\\IdeaProjects\\ficheros\\Ejercicio5", directorio);
         try {
             Files.createDirectory(p);
             System.out.println("El directorio se creará cuando se cierre la aplicación");
@@ -38,4 +55,46 @@ public class main {
             System.out.println(e.getMessage());
         }
     }
+    public static void crearFichero(String fichero){
+        Path p= Paths.get("C:\\Users\\alumno\\IdeaProjects\\ficheros\\Ejercicio5", fichero);
+        try {
+            Files.createFile(p);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        String texto = MiEntradaSalida.solicitarCadena("Introduce la  cadena de texto");
+
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(p,StandardOpenOption.APPEND)) {
+        bufferedWriter.write(texto);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void borrarFicheroODirectorio(String algo){
+
+        Path p = Paths.get("C:\\Users\\alumno\\IdeaProjects\\ficheros\\Ejercicio5",algo);
+        try {
+            Files.deleteIfExists(p);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void listarFicherosCarpeta(String carpeta){
+        Path p = Paths.get("C:\\Users\\alumno\\IdeaProjects\\ficheros\\Ejercicio5",carpeta);
+
+        try {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 }
